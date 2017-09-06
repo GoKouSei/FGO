dc40d9071d8abea735efabc93736fd689785c2c0
GoKouSei <gokousei030@gmail.com>
2017-09-06 18:07:16 +0800

    update layout

--- app/src/main/java/com/gokousei/fgo_tools/MainActivity.java
+++ app/src/main/java/com/gokousei/fgo_tools/MainActivity.java
@@ -13,14 +13,22 @@ import org.jsoup.select.Elements;
 
 public class MainActivity extends BaseActivity {
 
-    TextView tevGame;
-    String str="test";
+    TextView liftSlip_Tev_DogFoodCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        tevGame = findViewById(R.id.Main_tev_Game);
+        liftSlip_Tev_DogFoodCalculate = findViewById(R.id.leftSlip_Tev_DogFoodCalculate);
+        liftSlip_Tev_DogFoodCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View view1=findViewById(R.id.loadMainView);
+                view1.setVisibility(View.VISIBLE);
+                View view2=findViewById(R.id.tevTest);
+                view2.setVisibility(View.GONE);
+            }
+        });
     }
 
     public void loadData(View view) {
@@ -34,7 +42,7 @@ public class MainActivity extends BaseActivity {
                 Document doc = Jsoup.connect("http://fgowiki.com/").get();
                 Elements game = doc.select("div.header-container");
 //               str = game.get(0).select("ul").select("li").select("a").get(0).text();
-               str =BASE_URL+game.select("ul").select("li").select("a").get(0).attr("href");
+                String str = BASE_URL + game.select("ul").select("li").select("a").get(0).attr("href");
                 handler.sendEmptyMessage(0);
             } catch (Exception e) {
                 Log.d(LOG_TAG, e.toString());
