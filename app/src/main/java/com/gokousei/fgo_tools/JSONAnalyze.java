62c17ae6c7e162cd7f95651e3bfd4a3d4a80f653
GoKouSei <GoKouSei030@gmail.com>
2018-01-11 17:38:26 +0800

    update

--- app/src/main/java/com/gokousei/fgo_tools/JSONAnalyze.java
+++ app/src/main/java/com/gokousei/fgo_tools/JSONAnalyze.java
@@ -2,10 +2,6 @@ package com.gokousei.fgo_tools;
 
 import com.google.gson.Gson;
 
-/**
- * Created by z on 2017/12/27.
- */
-
 public class JSONAnalyze {
     Gson gson;
 }
--- app/src/main/java/com/gokousei/fgo_tools/ServantDataBean.java
+++ app/src/main/java/com/gokousei/fgo_tools/ServantDataBean.java
@@ -1,9 +1,5 @@
 package com.gokousei.fgo_tools;
 
-/**
- * Created by z on 2018/1/11.
- */
-
 public class ServantDataBean {
 
 }
